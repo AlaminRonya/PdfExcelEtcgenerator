8a00ded9e80791e312b0095378fec70c191e2cd2
AlaminRonya <hprony12@gmail.com>
2023-01-03 22:20:36 +0600

    pdf generator

---
+++ src/main/java/com/example/pdfexletcgenerator/PdfExlEtcGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdfexletcgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfExlEtcGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfExlEtcGeneratorApplication.class, args);
+    }
+
+}
